7e3007d07d809591bab80683be760d5ce0d54df3
Victor <V.Tokman@KOSTAL.COM>
2017-06-15 10:54:37 +0300

    Приховано розмітку Border.Sides

--- myPhotoEditor.Objects/Selection.cs
+++ myPhotoEditor.Objects/Selection.cs
@@ -557,14 +557,14 @@ namespace myPhotoEditor.Objects
                             g.DrawLine(pen, Location.X + Width / 2, Location.Y, Location.X + Width / 2, Location.Y + Height);
                             g.DrawRectangle(pen, Location.X, Location.Y, Width, Height);
                             
-                            foreach (BorderSide item in Border.Sides.Values)
-                            {
-                                if (item.MouseEntered)
-                                {
-                                    g.FillRectangle(Brushes.Lime, item.Region);
-                                }
-                                g.DrawRectangle(pen, item.Region);
-                            } 
+                            //foreach (BorderSide item in Border.Sides.Values)
+                            //{
+                            //    if (item.MouseEntered)
+                            //    {
+                            //        g.FillRectangle(Brushes.Lime, item.Region);
+                            //    }
+                            //    g.DrawRectangle(pen, item.Region);
+                            //} 
                                                        
                             break;
                     }
